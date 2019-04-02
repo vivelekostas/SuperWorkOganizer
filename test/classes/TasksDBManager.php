a4ca31c3b9b4ced0b6e4c77eb37cfb713b24b0f5
Костян <vivelakostas1096@gmail.com>
2019-04-02 16:03:39 +0300

    1)в DBManager доб-на ф. checkForTasks 2)test2 приспособлен под неё 3)создан config.php с константами

--- test/classes/TasksDBManager.php
+++ test/classes/TasksDBManager.php
@@ -1,10 +1,9 @@
 <?php
 
-
 class TasksDBManager {
 
     public function insertNewTask($newTask) {
-        $mysqli = new mysqli('localhost', 'mysql', 'mysql', 'kostas'); //ПОДКЛЮЧИЛИСЬ k DB KOSTAS
+        $mysqli = new mysqli(HOST, USERNAME, PASSWD, DBNAME); //ПОДКЛЮЧИЛИСЬ k DB KOSTAS
 
         $query = "INSERT INTO tasks VALUES (NULL, '"
                 . $newTask->getName() . "', '"
@@ -13,13 +12,13 @@ class TasksDBManager {
 
         $mysqli->query($query);
 
+        echo base64_decode('0JrQntCh0KLQryAtINCh0JrQntCX0JvQntCW0J7QnyEgINCR0JUt0JHQlS3QkdCVIQ==');
         $mysqli->close();
-        
     }
 
     public function createTableSql() {
-        $mysqli = new mysqli('localhost', 'mysql', 'mysql', 'kostas');
-        
+        $mysqli = new mysqli(HOST, USERNAME, PASSWD, DBNAME);
+
         $createTableSql = "CREATE TABLE
         `tasks` (
             `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
@@ -30,7 +29,24 @@ class TasksDBManager {
         );";
 
         $mysqli->query($createTableSql);
-        
+
+        $mysqli->close();
+    }
+
+    public function checkForTasks() {
+        $mysqli = new mysqli(HOST, USERNAME, PASSWD, DBNAME);
+        $query = "SHOW TABLES FROM `kostas`;";
+        $result = $mysqli->query($query);
+        $tables = mysqli_fetch_all($result, MYSQLI_NUM);    //получаю список всех т. из БД в виде массива
+
+
+        $contol = false;    //как бы "флажок"
+        foreach ($tables as $name) {
+            if (in_array("tasks", $name)) {    //ищет в массиве нужную т. "tasks"
+                $contol = true;    //активирует флажок если tasks уже существует
+            }
+        }
+        return $contol;    //возвращает true либо false  
         $mysqli->close();
     }
 
--- test/test2.php
+++ test/test2.php
@@ -3,6 +3,8 @@
 require_once ("./classes/Task.php");
 require_once ('./classes/TasksManager.php');
 require_once ('./classes/TasksDBManager.php');
+require_once ('./config.php');
+
 
 echo 'Хотите создать новую задачу? y/n: ';
 $task = readline();
@@ -25,25 +27,14 @@ if ($task === "y") {
 echo "Куда сохранить новую задачу: в DB или file? ";
 $save = readline();
 if ($save === "DB") {
-//  БЛОК ПРОВЕРКИ БД на существование т. "tasks" 
-    $mysqli = new mysqli('localhost', 'mysql', 'mysql', 'kostas'); 
-    $query = "SHOW TABLES FROM `kostas`;";
-    $result = $mysqli->query($query);
-    $tables = mysqli_fetch_all($result, MYSQLI_NUM);    //получаю список всех т. из БД в виде массива
+    $newCheckFor = new TasksDBManager();
+    $hasTask = $newCheckFor->checkForTasks();    //проверяет DB на наличие tasks, воз-щая true/false
 
-
-    $contol_array = array();    //как бы "флажок"
-    foreach ($tables as $name) {
-        if (in_array("tasks", $name)) {    //ищет в массиве нужную т. "tasks"
-            array_push($contol_array, "tasks");    //активирует флажок если tasks уже существует
-        }
-//  КОНЕЦ БЛОКА ПРОВЕРКИ БД
-        
-    } if ($contol_array == true) {    //вып-ся если флажок активен - просто добавляет нвоую задачу
+    if ($hasTask) {    //вып-ся если флажок активен (true) - просто добавляет новую задачу
         $newTasksDBManager = new TasksDBManager();
         $newTasksDBManager->insertNewTask($newTask);
         echo 'Задача успешно сохранена в DB';
-    } if ($contol_array == false) {    //вып-ся если флажок не активен - сначала создаёт т., а потом добавляет
+    } else {    //вып-ся если флажок не активен (false) - сначала создаёт т., а потом добавляет
         $newTable = new TasksDBManager();
         $newTable->createTableSql();
         $newTasksDBManager = new TasksDBManager();
